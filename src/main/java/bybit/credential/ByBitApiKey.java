8b0199ac9192175e10f8f804194cf59db07518c2
baongo97 <130117645+baongo97@users.noreply.github.com>
2025-03-31 19:25:18 +0800

    Update ByBitApiKey.java

--- src/main/java/bybit/credential/ByBitApiKey.java
+++ src/main/java/bybit/credential/ByBitApiKey.java
@@ -1,6 +1,7 @@
 package bybit.credential;
 
 public class ByBitApiKey {
-	public static final String API_KEY = "bTr91J9e4osVKvXJhH";
-	public static final String API_SECRETE = "5vY2e4Jkgg1caLbupKwoEUZ0Mfe4paIYqiJG";
+	// Input your API key here
+	public static final String API_KEY = "";
+	public static final String API_SECRETE = "";
 }
